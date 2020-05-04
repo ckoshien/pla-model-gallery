7c4dd794acd0e0a3b282eb5ee14145145638e7e5
ckoshien <ckoshien@gmail.com>
2020-05-05 00:09:01 +0900

    Updates

---
+++ precache-manifest.377e0e7783ba94bde0111793393d1155.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e960e0dee1c863b580a91de6e43019c8",
+    "url": "/pla-model-gallery/index.html"
+  },
+  {
+    "revision": "600fdd5cde729b3c5ee1",
+    "url": "/pla-model-gallery/static/css/main.a67dceed.chunk.css"
+  },
+  {
+    "revision": "5d32253b25a5326e9136",
+    "url": "/pla-model-gallery/static/js/2.f202d566.chunk.js"
+  },
+  {
+    "revision": "600fdd5cde729b3c5ee1",
+    "url": "/pla-model-gallery/static/js/main.c8afebb1.chunk.js"
+  },
+  {
+    "revision": "5492413edd67c8acd387",
+    "url": "/pla-model-gallery/static/js/runtime-main.577c6e2b.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/pla-model-gallery/precache-manifest.377e0e7783ba94bde0111793393d1155.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pla-model-gallery/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
