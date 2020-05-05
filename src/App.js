86075967f28119f8826cb0f8e2cb48f58f0f0357
ckoshien <ckoshien@gmail.com>
2020-05-05 12:27:53 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,221 +1,33 @@
 import React from "react";
 import "./App.css";
+import { data } from "./models";
 
 class App extends React.Component {
   state = {
-    gundam_seed:{
-      series:'機動戦士ガンダムSEED',
-      models:[
-        {
-          src:'https://bandai-hobby.net/images/153_880_s_438v1owxnhmx5s2z3iqreiwrlvay.jpg',
-          title: 'MG 1/100 GAT-X105 Strike'
-        },
-        {
-          src:'https://bandai-hobby.net/images/153_326_s_aio3s4xq0wlothvi9gh8wvp5carx.jpg',
-          title: '1/100 GAT-X303 Aegis',
-          built:false
-        },
-        {
-          src:'https://bandai-hobby.net/images/153_335_s_ppn8ht4jeclr10t0fyhofpse6bz6.jpg',
-          title: '1/100 GAT-X103 Buster',
-          built:false
-        },
-        {
-          src:'https://bandai-hobby.net/images/153_350_s_arrmrqkhfs99ahglsycbyab19i1q.jpg',
-          title: '1/100 GAT-X102 Duel Assault Shroud'
-        },
-        {
-          src:'https://bandai-hobby.net/images/153_392_s_enav1qgyd4r7ao22i2bn767fbqot.jpg',
-          title: '1/100 GAT-X207 Blitz',
-          built:false
-        },
-        {
-          src:'https://bandai-hobby.net/images/153_1502_s_iko0dwyhjs1ua6o0h5zpsqsfkfrc.jpg',
-          title: 'MG 1/100 ZGMF-X10A Freedom ver1.0'
-        },
-        {
-          src:'https://bandai-a.akamaihd.net/bc/img/model/b/1000136428_1.jpg',
-          title: 'RG 1/144 ZGMF-X10A Freedom',
-          built:false
-        },
-        {
-          src:'https://bandai-hobby.net/images/153_365_s_5uribxax1ysi5jdu7gwck9axuunr.jpg',
-          title: '1/100 ZGMF-X09A Justice',
-          built:false
-        },
-        {
-          src:'./img/zgmf-x09a_rg_144/CjDmf1FUoAANrwE.jpg',
-          title: 'RG 1/144 ZGMF-X09A Justice'
-        },
-        {
-          src:'https://bandai-hobby.net/images/153_1773_s_cdbkehcq8m9hp49wdijqrbvuagc2.jpg',
-          title: '1/100 ZGMF-X13A Providence'
-        },
-        {
-          src:'./img/gat-x131_144/P_20160918_222942_edit.jpg',
-          title: 'HG 1/144 GAT-X131 Calamity'
-        },
-        {
-          src:'./img/gat-x370_144/P_20160922_183201_edit.jpg',
-          title: 'HG 1/144 GAT-X370 Raider'
-        },
-        {
-          src:'https://bandai-hobby.net/images/153_2554_s_llxdpwcln9rckapec8c8mq0z2ut6.jpg',
-          title: 'MG 1/100 GAT-X105E STRIKE NOIR',
-          built:false
-        }
-      ]
-    },
-    gundam_seed_destiny:{
-      series:'機動戦士ガンダムSEED DESTINY',
-      models:[
-        {
-          src: "https://bandai-a.akamaihd.net/bc/img/model/b/1000136416_1.jpg",
-          title: "MG 1/100 ZGMF-X56S SWORD IMPULSE",
-          built:false
-        },
-        {
-          src: "./img/zgmf-x23s_100/main.jpg",
-          title: "1/100 ZGMF-X23S SAVIOUR",
-        },
-        {
-          src: "./img/zgmf-x24s_100/main.jpg",
-          title: "1/100 ZGMF-X24S CHAOS",
-        },
-        {
-          src: "./img/zgmf-x31s_144/main.jpg",
-          title: "HG 1/144 ZGMF-X31S ABYSS",
-        },
-        {
-          src: "./img/zgmf-1000_100/main.jpg",
-          title: "1/100 ZGMF-1000 ZAKU WARRIOR",
-        },
-        {
-          src: "./img/zgmf-x2000_144/main.jpg",
-          title: "1/100 ZGMF-X2000 GOUF IGNITED",
-        },
-        {
-          src: "./img/zgmf-x19a_100/main.jpg",
-          title: "1/100 ZGMF-X19A ∞JUSTICE",
-        },
-        {
-          src: "./img/zgmf-x666s_100/main.jpg",
-          title: "1/100 ZGMF-X666S LEGEND",
-        },
-        
-        {
-          src: "./img/zgmf-x20a_mg_100/main.jpg",
-          title: "MG 1/100 ZGMF-X20A STRIKE FREEDOM",
-        },
-        {
-          src: "https://bandai-hobby.net/images/153_973_s_7kvclzya4wkbhjzg1kyfe0mukytc.jpg",
-          title: "RG 1/144 ZGMF-X20A STRIKE FREEDOM",
-          built:false
-        },
-        {
-          src: "./img/zgmf-x20a_hgce_144/main.jpg",
-          title: "HGCE 1/144 ZGMF-X20A STRIKE FREEDOM"
-        },
-        {
-          src: "https://bandai-hobby.net/images/153_872_o_1ddksev8t1kb2fri1lij1ktu1f7u23.jpg",
-          title: "RG 1/144 ZGMF-X42S DESTINY",
-          built:false
-        },
-        {
-          src: "./img/zgmf-x42s_hgce_144/main.jpg",
-          title: "HGCE 1/144 ZGMF-X42S DESTINY",
-        },
-      ]
-    },
-    gundam_00_1st: {
-      series: "機動戦士ガンダム00 1st season",
-      models: [
-        {
-          src: "./img/gn-001_rg_144/P_20160821_212514_edit.jpg",
-          title: "RG 1/144 GN-001 Exia",
-        },
-        {
-          src: "./img/gn-002_100/CplcFjRVMAMiLWn.jpg",
-          title: "1/100 GN-002 Dynames",
-        },
-        {
-          src: "./img/gn-003_100/CpLbmwpUkAAR33n.jpg",
-          title: "1/100 GN-003 Kyrios",
-        },
-        {
-          src: "./img/gn-005_100/P_20160814_221712_1_edit.jpg",
-          title: "1/100 GN-005 Virtue",
-        },
-        {
-          src: "./img/gn-005_144/P_20180623_152714_1.jpg",
-          title: "HG 1/144 GN-005 Virtue",
-        },
-        {
-          src: "./img/gnw-001_144/P_20170528_225712.jpg",
-          title: "1/144 GNW-001 Throne Eins",
-        },
-        {
-          src: "./img/gnw-002_144/P_20170614_231850.jpg",
-          title: "1/144 GNW-002 Throne Zwei",
-        },
-        {
-          src: "./img/svms-01o/P_20170326_204214_edit.jpg",
-          title: "1/100 SVMS-01O Over Flag",
-        },
-        {
-          src: "https://bandai-hobby.net/images/155_1888_o_1bbm1dobjbnmahhbu556v1h282d.jpg",
-          title: "HG 1/144 GN-001/hs-A01D Avalanche",
-          built:false
-        }
-      ],
-    },
-    gundam_00_2nd:{
-      series:'機動戦士ガンダム00 2nd season',
-      models:[
-        {
-          src: "./img/gn-0000_rg_144/P_20160904_192250_edit.jpg",
-          title: "HG 1/144 GN-0000 00 Raiser",
-        },
-        {
-          src: "./img/gn-006_100/P_20160821_194018_1.jpg",
-          title: "1/100 GN-006 Cherudim",
-        },
-        {
-          src: "./img/gn-007_144/P_20180826_181938_1.jpg",
-          title: "HG 1/144 GN-007 Arios",
-        },
-        {
-          src: "./img/gn-008_100/P_20161204_201743_edit.jpg",
-          title: "1/100 GN-008 SERAVEE",
-        },
-        {
-          src: "./img/gn-008gnhwb_144/81640348_125288462296131_7026885342011029578_n.jpg",
-          title: "HG 1/144 GN-008 SERAVEE GNHW/B",
-        },
-        {
-          src: "./img/cb-0000g_144/P_20170701_210050.jpg",
-          title: "HG 1/144 CB-0000G/C Reborns",
-        },
-      ]
-    },
-    gundam_00_cinema:{
-      series:'機動戦士ガンダム00 劇場版',
-      models:[
-        {
-          src: "https://bandai-hobby.net/images/153_3686_o_1dt5bks323681r9p1h1eul7cj023.jpg",
-          title: "HG 1/144 GN-011 Harute",
-          built:false
-        }
-      ]
+    selectGrade: ["HG", "MG", "RG"],
+    selectScale: [100,144, "NON SCALE", 72, 48,60],
+  };
+
+  isDuplicate = (array, obj) => {
+    let newArray = array.filter((elm) => {
+      return elm === obj;
+    });
+    if (newArray.length > 0) {
+      return true;
+    } else {
+      return false;
     }
   };
 
   render() {
     let seriesList = [];
-    for(let key in this.state){
+    for (let key in data) {
       let modelList = [];
-      for (let i = 0; i < this.state[key].models.length; i++) {
-
+      let models = data[key].models.filter((elm) => {
+        return (this.state.selectGrade.includes(elm.grade)
+                && this.state.selectScale.includes(elm.scale))
+      });
+      for (let i = 0; i < models.length; i++) {
         modelList.push(
           <div
             style={{
@@ -231,11 +43,15 @@ class App extends React.Component {
                 width: 100,
               }}
             >
-              <img 
-                className={this.state[key].models[i].built !== false ? 'image_style' : 'image_style not_built'}
-                src={this.state[key].models[i].src}
+              <img
+                className={
+                  models[i].built !== false
+                    ? "image_style"
+                    : "image_style not_built"
+                }
+                src={models[i].src}
               />
-  
+
               <div
                 style={{
                   overflow: "hidden",
@@ -243,14 +59,28 @@ class App extends React.Component {
                   bottom: 0,
                   left: 0,
                   background: "rgba(140, 140, 140, 0.78)",
-                  fontSize: 14,
-                  //color:'white',
+                  fontSize: 15,
+                  color: "#d0d0d0",
                   fontWeight: "bold",
-                  height: 50,
+                  height: 70,
                   width: 100,
+                  textAlign: "center",
+                  textShadow: "1px 1px 1px black",
                 }}
               >
-                {this.state[key].models[i].title}
+                <div
+                  style={{
+                    margin: 5,
+                  }}
+                >
+                  <span className={"badge grade " + models[i].grade}>
+                    {models[i].grade}
+                  </span>
+                  <span className={"badge scale _" + models[i].scale}>
+                    1/{models[i].scale}
+                  </span>
+                </div>
+                {models[i].name}
               </div>
             </div>
           </div>
@@ -258,26 +88,130 @@ class App extends React.Component {
       }
       seriesList.push(
         <div>
-          <div className="series_title">{this.state[key].series}</div>
+          <div className="series_title">{data[key].series}</div>
           <div
             style={{
               display: "flex",
               flexWrap: "wrap",
-              margin:5
+              margin: 5,
             }}
-          >{modelList}</div>
+          >
+            {modelList}
+          </div>
         </div>
       );
     }
-    
+
+    let gChecks = [];
+    let sChecks = [];
+    let gradeChecks = [
+      {
+        label: "HG",
+      },
+      {
+        label: "MG",
+      },
+      {
+        label: "RG",
+      },
+    ];
+    let scaleChecks = [
+      {
+        label: 100,
+      },
+      {
+        label: 144,
+      },
+      {
+        label: 48,
+      },
+      {
+        label: 72,
+      },
+      {
+        label: "NON SCALE",
+      },
+      {
+        label: 60,
+      },
+    ];
+    for (let k = 0; k < gradeChecks.length; k++) {
+      gChecks.push(
+        <span>
+          <input
+            type="checkbox"
+            checked={this.state.selectGrade.includes(gradeChecks[k].label)}
+            onChange={() => {
+              let selectGrade = this.state.selectGrade;
+              let newArray = [];
+              if (this.isDuplicate(selectGrade, gradeChecks[k].label)) {
+                //要素を削除
+                newArray = selectGrade.filter((elm) => {
+                  return elm !== gradeChecks[k].label;
+                });
+                this.setState({
+                  selectGrade: newArray,
+                });
+              } else {
+                //要素を追加
+                selectGrade.push(gradeChecks[k].label);
+                this.setState({
+                  selectGrade: selectGrade,
+                });
+              }
+              console.log(this.state);
+            }}
+          />
+          {gradeChecks[k].label}
+        </span>
+      );
+    }
+    for (let l = 0; l < scaleChecks.length; l++) {
+      sChecks.push(
+        <span>
+          <input
+            type="checkbox"
+            checked={this.state.selectScale.includes(scaleChecks[l].label)}
+            onChange={() => {
+              let selectScale = this.state.selectScale;
+              let newArray = [];
+              if (this.isDuplicate(selectScale, scaleChecks[l].label)) {
+                //要素を削除
+                newArray = selectScale.filter((elm) => {
+                  return elm !== scaleChecks[l].label;
+                });
+                this.setState({
+                  selectScale: newArray,
+                });
+              } else {
+                //要素を追加
+                selectScale.push(scaleChecks[l].label);
+                this.setState({
+                  selectScale: selectScale,
+                });
+              }
+              console.log(this.state);
+            }}
+          />
+          1/{scaleChecks[l].label}
+        </span>
+      );
+    }
+
     return (
       <div
         style={{
           width: "100vw",
         }}
       >
-        <h1>お前の罪(積みプラ)を数えろ!</h1>
-        <div>ブラックアウトしているものは積んでいるか、かつて作って手放したもの</div>
+        <h1>お前の積みプラを数えろ!</h1>
+        <div>
+          ブラックアウトしているものは積んでいるか、
+          <br />
+          かつて作って手放したもの
+        </div>
+        <div>{gChecks}</div>
+        <div>{sChecks}</div>
         {seriesList}
       </div>
     );
---
+++ src/models.js
@@ -0,0 +1,293 @@
+export const data = {
+    
+    gundam_seed:{
+        series:'機動戦士ガンダムSEED',
+        models:[
+          {
+            src:'https://bandai-hobby.net/images/153_880_s_438v1owxnhmx5s2z3iqreiwrlvay.jpg',
+            name: 'GAT-X105 Strike',
+            grade:'MG',
+            scale:100,
+          },
+          {
+            src:'https://bandai-hobby.net/images/153_326_s_aio3s4xq0wlothvi9gh8wvp5carx.jpg',
+            name: 'GAT-X303 Aegis',
+            grade:'HG',
+            scale:100,
+            built:false
+          },
+          {
+            src:'https://bandai-hobby.net/images/153_335_s_ppn8ht4jeclr10t0fyhofpse6bz6.jpg',
+            name: 'GAT-X103 Buster',
+            grade:'HG',
+            scale:100,
+            built:false
+          },
+          {
+            src:'https://bandai-hobby.net/images/153_350_s_arrmrqkhfs99ahglsycbyab19i1q.jpg',
+            name: 'GAT-X102 Duel Assault Shroud',
+            grade:'HG',
+            scale:100,
+          },
+          {
+            src:'https://bandai-hobby.net/images/153_392_s_enav1qgyd4r7ao22i2bn767fbqot.jpg',
+            name: 'GAT-X207 Blitz',
+            grade:'HG',
+            scale:100,
+            built:false
+          },
+          {
+            src:'https://bandai-hobby.net/images/153_1502_s_iko0dwyhjs1ua6o0h5zpsqsfkfrc.jpg',
+            name: 'ZGMF-X10A Freedom ver1.0',
+            grade:'MG',
+            scale:100,
+          },
+          {
+            src:'https://bandai-a.akamaihd.net/bc/img/model/b/1000136428_1.jpg',
+            name: 'ZGMF-X10A Freedom',
+            grade:'RG',
+            scale:144,
+            built:false
+          },
+          {
+            src:'https://bandai-hobby.net/images/153_365_s_5uribxax1ysi5jdu7gwck9axuunr.jpg',
+            name: 'ZGMF-X09A Justice',
+            grade:'HG',
+            scale:100,
+            built:false
+          },
+          {
+            src:'./img/zgmf-x09a_rg_144/CjDmf1FUoAANrwE.jpg',
+            name: 'ZGMF-X09A Justice',
+            grade:'RG',
+            scale:144
+          },
+          {
+            src:'https://bandai-hobby.net/images/153_1773_s_cdbkehcq8m9hp49wdijqrbvuagc2.jpg',
+            name: 'ZGMF-X13A Providence',
+            grade: 'HG',
+            scale:100
+          },
+          {
+            src:'./img/gat-x131_144/P_20160918_222942_edit.jpg',
+            name: 'GAT-X131 Calamity',
+            grade:'HG',
+            scale:144
+          },
+          {
+            src:'./img/gat-x370_144/P_20160922_183201_edit.jpg',
+            name: 'GAT-X370 Raider',
+            grade:'HG',
+            scale:144
+          },
+          {
+            src:'https://bandai-hobby.net/images/153_2554_s_llxdpwcln9rckapec8c8mq0z2ut6.jpg',
+            name: 'GAT-X105E STRIKE NOIR',
+            grade:'MG',
+            scale:100,
+            built:false
+          }
+        ]
+      },
+      gundam_seed_destiny:{
+        series:'機動戦士ガンダムSEED DESTINY',
+        models:[
+          {
+            src: "https://bandai-a.akamaihd.net/bc/img/model/b/1000136416_1.jpg",
+            name: "ZGMF-X56S SWORD IMPULSE",
+            grade:'MG',
+            scale:100,
+            built:false
+          },
+          {
+            src: "./img/zgmf-x23s_100/main.jpg",
+            name: "ZGMF-X23S SAVIOUR",
+            grade:'HG',
+            scale:100
+          },
+          {
+            src: "./img/zgmf-x24s_100/main.jpg",
+            name: "ZGMF-X24S CHAOS",
+            grade:'HG',
+            scale:100
+          },
+          {
+            src: "./img/zgmf-x31s_144/main.jpg",
+            name: "ZGMF-X31S ABYSS",
+            grade:'HG',
+            scale:144
+          },
+          {
+            src: "./img/zgmf-1000_100/main.jpg",
+            name: "ZGMF-1000 ZAKU WARRIOR",
+            grade:'HG',
+            scale:100
+          },
+          {
+            src: "./img/zgmf-x2000_144/main.jpg",
+            name: "ZGMF-X2000 GOUF IGNITED",
+            grade:'HG',
+            scale:144
+          },
+          {
+            src: "./img/zgmf-x19a_100/main.jpg",
+            name: "ZGMF-X19A ∞JUSTICE",
+            grade:'HG',
+            scale:100
+          },
+          {
+            src: "./img/zgmf-x666s_100/main.jpg",
+            name: "ZGMF-X666S LEGEND",
+            grade:'HG',
+            scale:100
+          },
+          
+          {
+            src: "./img/zgmf-x20a_mg_100/main.jpg",
+            name: "ZGMF-X20A STRIKE FREEDOM",
+            grade:'MG',
+            scale:100
+          },
+          {
+            src: "https://bandai-hobby.net/images/153_973_s_7kvclzya4wkbhjzg1kyfe0mukytc.jpg",
+            name: "ZGMF-X20A STRIKE FREEDOM",
+            grade:'RG',
+            scale:144,
+            built:false
+          },
+          {
+            src: "./img/zgmf-x20a_hgce_144/main.jpg",
+            name: "ZGMF-X20A STRIKE FREEDOM",
+            grade:'HG',
+            scale:144
+          },
+          {
+            src: "https://bandai-hobby.net/images/153_872_o_1ddksev8t1kb2fri1lij1ktu1f7u23.jpg",
+            name: "ZGMF-X42S DESTINY",
+            grade:'RG',
+            scale:144,
+            built:false
+          },
+          {
+            src: "./img/zgmf-x42s_hgce_144/main.jpg",
+            name: "ZGMF-X42S DESTINY",
+            grade:'HG',
+            scale:144
+          },
+        ]
+      },
+      gundam_00_1st: {
+        series: "機動戦士ガンダム00 1st season",
+        models: [
+          {
+            src: "./img/gn-001_rg_144/P_20160821_212514_edit.jpg",
+            name: "GN-001 Exia",
+            grade:'RG',
+            scale:144
+          },
+          {
+            src: "./img/gn-002_100/CplcFjRVMAMiLWn.jpg",
+            name: "GN-002 Dynames",
+            grade:'HG',
+            scale:100
+          },
+          {
+            src: "./img/gn-003_100/CpLbmwpUkAAR33n.jpg",
+            name: "GN-003 Kyrios",
+            grade:'HG',
+            scale:100
+          },
+          {
+            src: "./img/gn-005_100/P_20160814_221712_1_edit.jpg",
+            name: "GN-005 Virtue",
+            grade:'HG',
+            scale:100
+          },
+          {
+            src: "./img/gn-005_144/P_20180623_152714_1.jpg",
+            name: "GN-005 Virtue",
+            grade:'HG',
+            scale:144
+          },
+          {
+            src: "./img/gnw-001_144/P_20170528_225712.jpg",
+            name: "GNW-001 Throne Eins",
+            grade:'HG',
+            scale:144
+          },
+          {
+            src: "./img/gnw-002_144/P_20170614_231850.jpg",
+            name: "GNW-002 Throne Zwei",
+            grade:'HG',
+            scale:144
+          },
+          {
+            src: "./img/svms-01o/P_20170326_204214_edit.jpg",
+            name: "1/100 SVMS-01O Over Flag",
+            grade:'HG',
+            scale:100
+          },
+          {
+            src: "https://bandai-hobby.net/images/155_1888_o_1bbm1dobjbnmahhbu556v1h282d.jpg",
+            name: "GN-001/hs-A01D Avalanche",
+            grade:'HG',
+            scale:144,
+            built:false
+          }
+        ],
+      },
+      gundam_00_2nd:{
+        series:'機動戦士ガンダム00 2nd season',
+        models:[
+          {
+            src: "./img/gn-0000_rg_144/P_20160904_192250_edit.jpg",
+            name: "GN-0000 00 Raiser",
+            grade:'RG',
+            scale:144
+          },
+          {
+            src: "./img/gn-006_100/P_20160821_194018_1.jpg",
+            name: "GN-006 Cherudim",
+            grade:'HG',
+            scale:100
+          },
+          {
+            src: "./img/gn-007_144/P_20180826_181938_1.jpg",
+            name: "HG 1/144 GN-007 Arios",
+            grade:'HG',
+            scale:144
+
+          },
+          {
+            src: "./img/gn-008_100/P_20161204_201743_edit.jpg",
+            name: "GN-008 SERAVEE",
+            grade:'HG',
+            scale:100
+          },
+          {
+            src: "./img/gn-008gnhwb_144/81640348_125288462296131_7026885342011029578_n.jpg",
+            name: "GN-008 SERAVEE GNHW/B",
+            grade:'HG',
+            scale:144
+          },
+          {
+            src: "./img/cb-0000g_144/P_20170701_210050.jpg",
+            name: "CB-0000G/C Reborns",
+            grade:'HG',
+            scale:144
+          },
+        ]
+      },
+      gundam_00_cinema:{
+        series:'機動戦士ガンダム00 劇場版',
+        models:[
+          {
+            src: "https://bandai-hobby.net/images/153_3686_o_1dt5bks323681r9p1h1eul7cj023.jpg",
+            name: "GN-011 Harute",
+            grade:'HG',
+            scale:144,
+            built:false
+          }
+        ]
+      }
+}
